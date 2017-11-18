12214f9b0e1877a43c6a3dc10060e721bc3fb9c4
marcosjrs <marcosjrs80@gmail.com>
2017-11-18 10:24:14 +0100

    Creacion componentes principales.  Y vinculacion a los links mediante componentes Route.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {BrowserRouter as Router, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import Home from './principal/Home';
+import Login from './principal/Login';
+import Signup from './principal/Signup';
 
 const Header = () => {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/">Sign Up</Link>
-      <Link to="/">Login</Link>
+      <Link to="/signup">Sign Up</Link>
+      <Link to="/login">Login</Link>
     </nav>
   );
 };
@@ -15,7 +19,12 @@ const Header = () => {
 const App = () => {
   return (
     <Router>
+      <div>
       <Header />
+      <Route exact path="/" component={Home}/>
+      <Route path="/signup" component={Signup}/>
+      <Route path="/login" component={Login}/>
+      </div>
     </Router>
   );
 };
